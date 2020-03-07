cba4f6254477e2cccccf918fb74a06732c104890
Rayesh <rayeshrajah@gmail.com>
2020-03-07 17:09:33 +0000

    Made some more fake data

--- db/seeds/wiki_fake_seeds.sql
+++ db/seeds/wiki_fake_seeds.sql
@@ -2,3 +2,10 @@ INSERT INTO users(first_name, last_name, password, email, avatar, location)
 VALUES ('Rayesh','Rajah', 'password',  'ray_6785876@hotmail.com', 'some picture', 'Montreal'),
        ('Felix', 'Rioux', 'password', 'something@gmail.com', 'some picture', 'Montreal'),
        ('Camilo','Pineda','password', 'something@hotmail.com', 'some picture', 'Montreal');
+
+INSERT INTO maps (name, latitude, longitude, zoom) 
+VALUES ('Amazing map', 45.5017, -73.5673), 
+       ('Boring map', 45.5017, -73.5673),
+       ('Cool map', 45.5017, -73.5673);
+
+INSERT INTO 
