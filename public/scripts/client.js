281a9e5b44e17c73a3ba00a18823a8170dd50b6e
DragonMess <kmylop@hotmail.com>
2020-03-10 14:39:15 +0000

    add some points

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,58 +5,46 @@ $("#map-form").on("submit", function(event) {
 });
 
 const renderMaps = function(maps) {
-    $('#maps-container').empty();
-    for(let i = 0; i < maps.length; i++){
-     $map = createMapElement(maps[i]);
-     $('#maps-container').prepend($map);
-    }
-  };
+  $("#maps-container").empty();
+  for (let i = 0; i < maps.length; i++) {
+    const $map = createMapElement(maps[i]);
+    $("#maps-container").prepend($map);
+  }
+};
 
-
-  function createMapElement(dataMap) {
-    let $map = `
-<<<<<<< HEAD
+const createMapElement = (dataMap) => {
+  let $map = `
     <div  class="map-list-item point">
 <img src="/assets/img/compass.svg" alt="" style="width: 3em" title="compass">
       <p class="elementMap"  id=${dataMap.id}>
         ${dataMap.name} 
       </p>
     </div>`;
-    return $map;
-  }
-=======
-    <div class="map-list-item">
-      <button>
-        <img src="/assets/img/compass.svg" alt="" style="width: 3em" title="compass">
-        <p id=${dataMap.id}>
-          ${dataMap.name}
-        </p>
-      </button>
-    </div>`;
-    return $map;
-  }
+  return $map;
+}
 
->>>>>>> 5787acbe934c3172d14a4bc89cf7cd6f91575650
-  function loadMaps() {
-    $.ajax({
-      method: "GET",
-      url: `/maps`,
-    }).done(renderMaps);
-  }
-  loadMaps();
+const loadMaps = () =>{
+  $.ajax({
+    method: "GET",
+    url: `/maps`
+  }).done(renderMaps);
+};
+loadMaps();
 
 const postMap = function(event) {
-    $.ajax({
+  $.ajax({
     method: "POST",
     url: `/maps`,
-    data: $("#map-form").serialize(),
-    }).done(() => loadMaps());
+    data: $("#map-form").serialize()
+  }).done(() => loadMaps());
 };
 // have id from the cliked map name
 let idMap;
 $("map-list-item").on("click", ".point", function(e) {
   console.log(e.target);
-  idMap = $(e.target).siblings(".elementMap").attr("id");
+  idMap = $(e.target)
+    .siblings(".elementMap")
+    .attr("id");
   console.log($(e.target).children(".elementMap"));
 });
 
