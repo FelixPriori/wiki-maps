0687571017318dccee496a7c1b0404ebf004e09e
Rayesh <rayeshrajah@gmail.com>
2020-03-12 16:52:32 +0000

    Added cookies

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,23 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 const userHelper = require('../lib/db_helper');
-
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
   router.post("/register", (req, res) => {
     let userData = req.body;
-    console.log(userData);
+    req.session.userID = //enter the userId that the database has
     userData = {
       first_name: userData.first_name,
       email: userData.email,
-      password: userData.password
+      password: userData.password,
     };
-    userHelper.addUser(db, userData).then(dbRes => res.json(dbRes));
+    userHelper.addUser(db, userData).then((dbRes) => {
+      req.session.userID = dbRes.id
+      console.log(req.session.userID);
+      res.json(dbRes)
+    });
   });
 
   router.post("/login", (req, res) => {
